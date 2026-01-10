12c46cc3c78250d105423fdb145c9eead7fc2c43
berke <soysalberke.96@gmail.com>
2026-01-10 22:39:50 +0100

    Fix socket.io config for fly.io WebSocket support

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  },
+  transports: ['websocket', 'polling'],
+  allowUpgrades: true,
+  pingTimeout: 60000,
+  pingInterval: 25000
+});
 
 app.use(express.static('public'));
 
